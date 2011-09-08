81029c14f13e18c8ed2a127b930b8c74ed5f5bb5
fengkuok@gmail.com <fengkuok@gmail.com@cb3cb32d-09c6-85e5-b2c2-c33622451f53>
2011-09-08 13:44:53 +0000

    pom parent修正

---
+++ SysPeak/apps/MakerEAP/workflow/src/main/webapp/app/controller/Users.js
@@ -0,0 +1,25 @@
+Ext.define('AM.controller.Users', {
+    extend: 'Ext.app.Controller',
+
+    views: [
+        'user.List',
+		'user.Edit'
+    ],
+    stores: [
+        'Users'
+    ],
+
+    init: function() {
+        this.control({
+            'userlist': {
+                itemdblclick: this.editUser
+            }
+        });
+    },
+
+	editUser: function(grid, record) {
+        var view = Ext.widget('useredit');
+
+        view.down('form').loadRecord(record);
+    }
+});
--- SysPeak/apps/MakerEAP/workflow/src/main/webapp/index.jsp
+++ SysPeak/apps/MakerEAP/workflow/src/main/webapp/index.jsp
@@ -1,5 +1,10 @@
 <html>
-<body>
-<h2>Hello World!</h2>
-</body>
+<head>
+    <title>Hello Ext</title>
+
+    <link rel="stylesheet" type="text/css" href="extjs/resources/css/ext-all.css">
+    <script type="text/javascript" src="extjs/ext-debug.js"></script>
+    <script type="text/javascript" src="app.js"></script>
+</head>
+<body></body>
 </html>
