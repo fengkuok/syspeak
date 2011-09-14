f07896bb81ad287bd64013d2d31167159e78001d
fengkuok@gmail.com <fengkuok@gmail.com@cb3cb32d-09c6-85e5-b2c2-c33622451f53>
2011-09-14 16:23:30 +0000

    初始导入。

--- SysPeak/apps/MakerEAP/workflow/src/main/webapp/app.js
+++ SysPeak/apps/MakerEAP/workflow/src/main/webapp/app.js
@@ -1,21 +1,24 @@
-Ext.application({
-    name: 'AM',
+/**
+ * Copy Right SysPeak @ 2011
+ * @author Feng Kuok
+ */
 
-    appFolder: 'app',
-
-    controllers: [
-        'Users'
-    ],
-
-    launch: function() {
-        Ext.create('Ext.container.Viewport', {
-            layout: 'fit',
-            items: [
-                {
-                    xtype: 'userlist'
-                }
-            ]
-        });
+Ext.Loader.setConfig({
+    enabled: true,
+    paths: {
+        'Workflow': './app',
+        'cxt' : 'http://localhost:8080/maker-workflow'
     }
+});
+
+Ext.require('Workflow.Application');
+//发现用动态加载后竟然不能直接用Ext.Msg.XX方法，暂时解决办法如下：
+Ext.require('Ext.window.MessageBox');
+
+Ext.onReady(function() {
+	//Ext core没会使用，很惭愧；以后会把所有的document.XXX改为Ext core来实现。
+	//Ext.core.DomHelper.useDom = true;
+	document.getElementById('loading-msg').innerHTML = '模块初始化...';
+    Ext.create('Workflow.Application');
 });
 
--- SysPeak/apps/MakerEAP/workflow/src/main/webapp/index.jsp
+++ SysPeak/apps/MakerEAP/workflow/src/main/webapp/index.jsp
@@ -1,10 +1,18 @@
+<!DOCTYPE html>
 <html>
 <head>
-    <title>Hello Ext</title>
+  <title>Ext JS 4.0.3 API Documentation</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="canonical" href="http://docs.sencha.com/ext-js/4-0/"/>
 
-    <link rel="stylesheet" type="text/css" href="extjs/resources/css/ext-all.css">
-    <script type="text/javascript" src="extjs/ext-debug.js"></script>
-    <script type="text/javascript" src="app.js"></script>
+  <script type="text/javascript" src="static/extjs4/ext-all.js"></script>
+
+  <script type="text/javascript"> Ext.ns("Docs"); </script>
+  <script type="text/javascript" src="app.js"></script>
+
+
 </head>
-<body></body>
+<body id="ext-body" class="iScroll">
+
+</body>
 </html>
