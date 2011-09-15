fe5eb2b4177bd418ffce7399458416e1e0526942
fengkuok@gmail.com <fengkuok@gmail.com@cb3cb32d-09c6-85e5-b2c2-c33622451f53>
2011-09-15 15:46:17 +0000

    alpha，供大家预览，代码会重构

--- SysPeak/apps/MakerEAP/workflow/src/main/webapp/app.js
+++ SysPeak/apps/MakerEAP/workflow/src/main/webapp/app.js
@@ -13,9 +13,15 @@ Ext.Loader.setConfig({
 
 Ext.require('Workflow.Application');
 //发现用动态加载后竟然不能直接用Ext.Msg.XX方法，暂时解决办法如下：
-Ext.require('Ext.window.MessageBox');
+Ext.require([
+	'Ext.window.MessageBox',
+	'Ext.tip.*',
+	'Ext.JSON.*',
+	'Ext.Object.*'
+]);
 
 Ext.onReady(function() {
+	Ext.QuickTips.init();
 	//Ext core没会使用，很惭愧；以后会把所有的document.XXX改为Ext core来实现。
 	//Ext.core.DomHelper.useDom = true;
 	document.getElementById('loading-msg').innerHTML = '模块初始化...';
