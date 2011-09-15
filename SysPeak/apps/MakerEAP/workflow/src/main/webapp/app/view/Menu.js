3e92b21d6b71bc7fda7b7545aa113342568e8636
fengkuok@gmail.com <fengkuok@gmail.com@cb3cb32d-09c6-85e5-b2c2-c33622451f53>
2011-09-15 15:45:50 +0000

    alpha，供大家预览，代码会重构

--- SysPeak/apps/MakerEAP/workflow/src/main/webapp/app/view/Menu.js
+++ SysPeak/apps/MakerEAP/workflow/src/main/webapp/app/view/Menu.js
@@ -1,5 +1,9 @@
 Ext.define('Workflow.view.Menu',{
     extend: 'Ext.panel.Panel',
+    requires :[
+    	'Workflow.view.Tree'
+    ],
+    
     region:'west',
     layout: 'accordion',
     minWidth : 130,
@@ -11,7 +15,7 @@ Ext.define('Workflow.view.Menu',{
     defaultMargins : {top:0, right:0, bottom:-1, left:1},
 	defaults: {
         // applied to each contained panel
-        bodyStyle: 'padding:15px'
+        bodyStyle: 'padding:0px'
     },
     layoutConfig: {
         // layout-specific configs go here
@@ -27,20 +31,22 @@ Ext.define('Workflow.view.Menu',{
     		iconCls:'icon-menu',
     		width : 200
         });
-        
-        this.items = [{
-	        title: 'Panel 1',
-	        iconCls : 'icon-tree-accordion',
-	        html: 'Panel content!'
-	    },{
-	        title: 'Panel 2',
-	        iconCls : 'icon-tree-accordion',
-	        html: 'Panel content!'
-	    },{
-	        title: 'Panel 3',
-	        iconCls : 'icon-tree-accordion',
-	        html: 'Panel content!'
-	    }];
+        this.loadMenus();
+//        this.items = [{
+//	        title: 'Panel 1',
+//	        iconCls : 'icon-tree-accordion',
+//	        items : [
+//	        	
+//	        ]
+//	    },{
+//	        title: 'Panel 2',
+//	        iconCls : 'icon-tree-accordion',
+//	        html: 'Panel content!'
+//	    },{
+//	        title: 'Panel 3',
+//	        iconCls : 'icon-tree-accordion',
+//	        html: 'Panel content!'
+//	    }];
 	    
 	    this.bbar = [{
         	text : '开始',
@@ -91,5 +97,54 @@ Ext.define('Workflow.view.Menu',{
         }]
 	    
         this.callParent(arguments);
+    },
+    
+    buildMenuTree : function(trees){
+		
+		alert(trees);
+    },
+    
+    loadMenus : function(){
+    	Ext.Ajax.request({
+		    url: 'data/tree.json',
+		    params: {
+		        id: 1
+		    },
+		    scope: this,
+		    success: function(response){
+		        var text = response.responseText;
+		        var trees = Ext.JSON.decode(text);
+		        //alert(trees.length);
+		        Ext.Object.each(trees,function(key, value, myself){
+		        	//alert(value.id)
+		        	//alert(key + ":" + value)
+		        	//alert(Ext.JSON.encode(value.children));
+		        	var tree = Ext.create('Ext.tree.Panel', {
+				        title: value.text,
+				        margins : '0 0 -1 1',
+				        useArrows: true,
+				        iconCls: value.iconCls,
+			            rootVisible: false,
+			            border : true,
+			            split: true,
+            			collapsible : true,
+			            defaults :{
+			            	expanded : true
+			            },
+			            //loader: new Ext.tree.TreeLoader(),
+			            root: {
+					        //text: "Root node",
+					        expanded: true,
+					        children: value.children
+					    }
+			            //root: Ext.JSON.encode(value.children)
+				    });
+				    //发现ext4中已经去除了createDelegate方法，所以只能是scope中指定为this了（不知道是不是用别方法替代了，很奇怪为什么createDelegate不见了）
+				    this.add(tree); //this 指的是menu-panel
+				    //alert(this.getItemId())
+				    //alert(Ext.getCmp('menu-panel').getItemId());
+		        },this);
+		    }
+		});
     }
 });
