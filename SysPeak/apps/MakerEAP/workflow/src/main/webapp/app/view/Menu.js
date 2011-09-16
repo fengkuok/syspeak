ec80ec4686a1beaeb6297f179dcf9d64ca9d263c
fengkuok@gmail.com <fengkuok@gmail.com@cb3cb32d-09c6-85e5-b2c2-c33622451f53>
2011-09-16 16:33:49 +0000

    alpha

--- SysPeak/apps/MakerEAP/workflow/src/main/webapp/app/view/Menu.js
+++ SysPeak/apps/MakerEAP/workflow/src/main/webapp/app/view/Menu.js
@@ -1,70 +1,111 @@
-Ext.define('Workflow.view.Menu', {
-	extend : 'Ext.panel.Panel',
-	requires : [ 'Workflow.view.Tree' ],
-
-	id : 'menu',
-	region : 'west',
-	layout : 'accordion',
-	minWidth : 130,
-	maxWidth : 280,
-	split : true,
-	collapsible : true,
-	border : false,
-
-	defaultMargins : {
-		top : 0,
-		right : 0,
-		bottom : -1,
-		left : 1
-	},
-	defaults : {
-		// applied to each contained panel
-		bodyStyle : 'padding:0px'
-	},
-	layoutConfig : {
-		// layout-specific configs go here
-		titleCollapse : true,
-		animate : true,
-		activeOnTop : true
-	},
-
-	initComponent : function() {
-		Ext.apply(this, {
-			id : 'menu-panel',
-			title : '当前用户:FengKuok',
-			iconCls : 'icon-menu',
-			width : 200
+Ext.define('Workflow.view.Menu',{
+    extend: 'Ext.panel.Panel',
+    
+    region:'west',
+    layout: 'accordion',
+    minWidth : 130,
+    maxWidth : 280,
+    split: true,
+    collapsible: true,
+    border: false,
+    
+    defaultMargins : {top:0, right:0, bottom:-1, left:1},
+	defaults: {
+        // applied to each contained panel
+        bodyStyle: 'padding:0px'
+    },
+    layoutConfig: {
+        // layout-specific configs go here
+        titleCollapse: true,
+        animate: true,
+        activeOnTop: true
+    },    
+    
+    initComponent : function(){
+        Ext.applyIf(this,{
+            id: 'menu-panel',
+    		title: '当前用户:FengKuok',
+    		iconCls:'icon-menu',
+    		width : 200
+        });
+        
+        this.loadMenus();
+        //bbar暂时有点问题
+//        this.buildBottomBar();
+	    
+        this.callParent(arguments);
+    },
+    
+    //private
+    buildMenuTree : function(trees){
+		Ext.Object.each(trees,function(key, value, myself){
+        	var tree = Ext.create('Workflow.view.cls.Tree', {
+		        title: value.text,
+		        iconCls: value.iconCls,
+	            root: {
+			        children: value.children
+			    }
+		    });
+		    
+		    tree.on('itemclick',function(view, record, item, index){
+    	    	if(!record.raw.isClass){
+		    		return;
+		    	}
+		    	//在center-panel中打开
+		    	this.setActiveTab(record);
+		    },this);
+		    //发现ext4中已经去除了createDelegate方法，所以只能是scope中指定为this了（不知道是不是用别方法替代了，很奇怪为什么createDelegate不见了）
+		    this.add(tree); //this 指的是menu-panel
+        },this);
+        
+        this.doLayout();
+    },
+    
+    //private
+    setActiveTab : function(record){
+    	var tabs = Ext.getCmp('content-panel');
+    	//id默认为数据库中的主键，这个以后会更改更长一些的数据。
+    	var tabItem = tabs.getComponent(record.data.id);
+    	
+    	if(tabItem == 'undefined' || tabItem == null){
+    		var p = Ext.create(record.raw.clsName,{
+	    		title : record.data.text,
+	    		iconCls : record.data.iconCls,
+	    		closable : true,
+	    		id : record.data.id
+	    	});
+	    	tabItem = tabs.add(p);
+    	}
+    	tabs.setActiveTab(tabItem);
+    },
+    
+    loadMenus : function(){
+    	Ext.Ajax.request({
+		    url: 'data/tree.json',
+		    scope: this,
+		    params: {
+		        id: 1
+		    },
+		    success: function(response){
+		        var trees = Ext.JSON.decode(response.responseText);
+		        this.buildMenuTree(trees);
+		    }
 		});
-		this.loadMenus();
-		// this.items = [{
-		// title: 'Panel 1',
-		// iconCls : 'icon-tree-accordion',
-		// items : [
-		//	        	
-		// ]
-		// },{
-		// title: 'Panel 2',
-		// iconCls : 'icon-tree-accordion',
-		// html: 'Panel content!'
-		// },{
-		// title: 'Panel 3',
-		// iconCls : 'icon-tree-accordion',
-		// html: 'Panel content!'
-		// }];
-
-		this.bbar = [ {
-			text : '开始',
-			iconCls : 'icon-start',
-			menu : {
-				xtype : 'menu',
-				items : [ {
+    },
+    
+    buildBottomBar : function(){
+		this.bbar = [{
+        	text : '开始',
+        	iconCls : 'icon-start',
+        	menu : {
+        		xtype : 'menu',
+				items : [{
 					text : '返回首页',
 					iconCls : 'icon-home',
 					handler : function(n) {
 						try {
-							// Ext.getCmp('content-panel').layout.setActiveItem('start-panel');
-						} catch (e) {
-						}
+							//Ext.getCmp('content-panel').layout.setActiveItem('start-panel');
+						} catch (e) {}
 					}
 				}, {
 					text : '关于系统',
@@ -78,65 +119,50 @@ Ext.define('Workflow.view.Menu', {
 					handler : function(n) {
 						Ext.Msg.alert('联系我们', 'QQ群：19564395');
 					}
-				} ]
-			}
-		}];
-
-		this.callParent(arguments);
-	},
-
-	buildMenuTree : function(trees) {
-
-		alert(trees);
-	},
-
-	loadMenus : function() {
-		Ext.Ajax.request({
-			url : 'data/tree.json',
-			params : {
-				id : 1
-			},
-			scope : this,
-			success : function(response) {
-				var text = response.responseText;
-				var trees = Ext.JSON.decode(text);
-//				 alert(trees.length);
-				Ext.Object.each(trees, function(key, value, myself) {
-//					 alert(value.id)
-//					 alert(key + ":" + value)
-					// alert(Ext.JSON.encode(value.children));
-					var tree = Ext.create('Ext.tree.Panel', {
-						title : value.text,
-						margins : '0 0 -1 1',
-						useArrows : true,
-						iconCls : value.iconCls,
-						rootVisible : false,
-						border : true,
-						split : true,
-						collapsible : true,
-						defaults : {
-							expanded : true
-						},
-						// loader: new Ext.tree.TreeLoader(),
-						root : {
-							// text: "Root node",
-							expanded : true,
-							children : value.children
-						},
-						listeners : {
-							itemclick : function() {
-								Ext.Msg.alert('fuck you', 'bitches');
-							}
-						}
-
-					// root: Ext.JSON.encode(value.children)
-					});
-					// 发现ext4中已经去除了createDelegate方法，所以只能是scope中指定为this了（不知道是不是用别方法替代了，很奇怪为什么createDelegate不见了）
-					this.add(tree); // this 指的是menu-panel
-					// alert(this.getItemId())
-					// alert(Ext.getCmp('menu-panel').getItemId());
-				}, this);
-			}
-		});
-	}
+				}]
+        	}
+        }, '->', {
+        	xtype: 'combo',
+        	fieldLabel: '主题',
+		    store: Ext.create('Ext.data.Store', {
+			    fields: ['abbr', 'name'],
+			    data : [
+			        {"abbr":"AL", "name":"Alabama"},
+			        {"abbr":"AK", "name":"Alaska"},
+			        {"abbr":"AZ", "name":"Arizona"}
+			        //...
+			    ]
+			}),
+			labelAlign: 'left',
+			labelWidth: 32,
+			width: 100,
+		    queryMode: 'local',
+		    displayField: 'name',
+		    valueField: 'abbr'
+        	
+        }];
+    }
 });
+
+//    allowDrag: true
+//allowDrop: true
+//checked: null
+//cls: ""
+//depth: 4
+//expandable: true
+//expanded: false
+//href: ""
+//hrefTarget: ""
+//iconCls: "icon-tree-cassette"
+//id: ""
+//index: 0
+//isFirst: true
+//isLast: true
+//leaf: true
+//loaded: false
+//loading: false
+//parentId: "4"
+//qtip: ""
+//qtitle: ""
+//root: false
+//text: "SuperJsonGrid1"
