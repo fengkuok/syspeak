3b169d694fd959aa052dbc2b60ee2b5d9f75e45b
fengkuok@gmail.com <fengkuok@gmail.com@cb3cb32d-09c6-85e5-b2c2-c33622451f53>
2011-09-27 13:31:19 +0000

    预览

--- SysPeak/apps/MakerEAP/workflow/src/main/webapp/app/store/Users.js
+++ SysPeak/apps/MakerEAP/workflow/src/main/webapp/app/store/Users.js
@@ -1,22 +1,21 @@
 Ext.define('Workflow.store.Users', {
-	extend : 'Ext.data.Store',
-	fields : [ 'name', 'email' ],
-	// data: [
-	// {name: 'Ed', email: 'ed@sencha.com'},
-	// {name: 'Tommy', email: 'tommy@sencha.com'}
-	// ],
-
-	pageSize : 3,
-	proxy : {
-		type : 'ajax',
-		url : 'user/list',
-		reader : {
-			type : 'json',
-			root : 'users',
-			totalProperty : 'total'
-		}
-
-	},
-
-	autoLoad : true
+    extend: 'Workflow.store.cls.Store',
+    
+    model: 'Workflow.model.User',
+    //autoLoad: true,
+	readUrl: 'probe/processengine',//'resources/data/users.js',
+    updateUrl: 'resources/data/updateUsers.js'
+//    proxy: {
+//        type: 'ajax',
+//	    api: {
+//	        read: 'data/users.json',
+//	        update: 'data/updateUsers.json'
+//	    },
+//	    reader: {
+//	        type: 'json',
+//	        root: 'users',
+//	        //totalProperty  : 'total',
+//	        successProperty: 'success'
+//	    }
+//    }
 });
