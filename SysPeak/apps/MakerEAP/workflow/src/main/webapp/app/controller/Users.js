05c3799dade27037eed4d4d256e7887a48744727
fengkuok@gmail.com <fengkuok@gmail.com@cb3cb32d-09c6-85e5-b2c2-c33622451f53>
2011-09-27 13:30:23 +0000

    预览

--- SysPeak/apps/MakerEAP/workflow/src/main/webapp/app/controller/Users.js
+++ SysPeak/apps/MakerEAP/workflow/src/main/webapp/app/controller/Users.js
@@ -8,18 +8,34 @@ Ext.define('Workflow.controller.Users', {
     stores: [
         'Users'
     ],
+    models: [
+    	'User'
+    ],
 
     init: function() {
-        this.control({
-            'userlist': {
-                itemdblclick: this.editUser
-            }
-        });
+//    	this.control({
+//            'userlist': {
+//                itemdblclick: this.editUser
+//            },
+//            'useredit button[action=save]': {
+//                click: this.updateUser
+//            }
+//        });
     },
 
 	editUser: function(grid, record) {
         var view = Ext.widget('useredit');
 
         view.down('form').loadRecord(record);
+    },
+    updateUser: function(button) {
+       	var win    = button.up('window'),
+        form   = win.down('form'),
+        record = form.getRecord(),
+        values = form.getValues();
+
+    	record.set(values);
+    	win.close();
+    	this.getUsersStore().sync();
     }
 });
---
+++ SysPeak/apps/MakerEAP/workflow/src/main/webapp/app/controller/cls/Controller.js
@@ -0,0 +1,26 @@
+Ext.define('Workflow.controller.cls.Controller', {
+    extend: 'Ext.app.Controller',
+
+    initComponent: function() {
+    	Ext.applyIf(this,{
+    	
+    	});
+    },
+    
+    createModel : function(){
+    
+    },
+    
+    retrieveModel : function(){
+    
+    },
+    
+    updateModel : function(grid, record){
+    
+    },
+    
+    deleteModel : function(){
+    
+    }
+    
+});
