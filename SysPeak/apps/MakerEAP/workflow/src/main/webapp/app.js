91e67ea8ddbba272aebb2aada599b5e240b526c1
fengkuok@gmail.com <fengkuok@gmail.com@cb3cb32d-09c6-85e5-b2c2-c33622451f53>
2011-09-27 12:49:51 +0000

    更新Provider

--- SysPeak/apps/MakerEAP/workflow/src/main/webapp/app.js
+++ SysPeak/apps/MakerEAP/workflow/src/main/webapp/app.js
@@ -6,7 +6,9 @@ Ext.Loader.setConfig({
     enabled: true,
     paths: {
     	'cxt' : '.',		//content path
-        'Workflow': './app' 
+    	'SysPeak' : './resources/scripts/ux/syspeak',
+        'Workflow': './app'
+        
     }
 });
 
@@ -15,6 +17,9 @@ Ext.require([
 	'Ext.tip.*',
 	'Ext.JSON.*',
 	'Ext.Object.*',
+	'Ext.view.View',//LocalStorageProvider need
+	'Ext.grid.Panel',//LocalStorageProvider need
+	'Ext.form.field.Base',//LocalStorageProvider need
 	'Ext.picker.Date',
 	'Workflow.Application'
 ]);
@@ -24,17 +29,29 @@ Ext.onReady(function() {
 	Ext.BLANK_IMAGE_URL = Ext.Loader.getPath('cxt') + '/static/extjs4/resources/themes/images/default/s.gif';
 	//Ext.QuickTips.init();
 	
-	if(window.localStorage){
- 		var theme = localStorage.themeLocalStore;
- 		if(theme == null || theme == "null" || theme == 'undefined'){
- 			theme = "";
- 		}
- 		Ext.util.CSS.swapStyleSheet("theme", Ext.Loader.getPath('cxt') + '/static/extjs4/resources/css/ext-all' + theme + '.css');
-	}else{
-	 	alert('This browser does NOT support localStorage');
-	}
+	/**
+	 *  设置默认Provider
+	 *  正常是在Application中定义的，但影响默认换肤效果，所以提前设置；
+	 *  引入的问题是需要在之前require以下3个类
+	 * 'Ext.view.View','Ext.grid.Panel','Ext.form.field.Base'
+	 * 
+	 */
+	if (window.localStorage) {
+        Ext.state.Manager.setProvider(Ext.create('Ext.state.LocalStorageProvider'));
+    } else {
+        Ext.state.Manager.setProvider(Ext.create('Ext.state.CookieProvider'));
+    }
+	
+	var theme = Ext.state.Manager.get('theme');
+	theme = typeof theme == 'undefined' ? '' : theme;
+	//换肤
+	Ext.util.CSS.swapStyleSheet("theme", Ext.Loader.getPath('cxt') + '/static/extjs4/resources/css/ext-all' + theme + '.css');
+    
 	
 	Ext.get('loading-msg').update('模块初始化...');
+	
     Ext.create('Workflow.Application');
+    
+	delete theme;
 });
 
